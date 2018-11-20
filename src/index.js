07624b59002aa17d25b07adb4029f88c9289b456
Irakli Gozalishvili <rfobic@gmail.com>
2018-11-19 23:20:35 -0800

    Make resource fetcher configurable

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ import createSingleFile from './create-single-file.js'
  * Note this is an unstandardised workaround to keep URLs of subresources available; unfortunately
  * URLs inside stylesheets are still lost.
  * @param {Date} [options.now] - Override the snapshot time (only relevant when addMetadata=true).
+ * @param {Function} [options.fetchResource] - function API compatible with fetch (default) for fetching resources.
  * @returns {string} html - The freeze-dried document as a self-contained, static string of HTML.
  */
 export default async function freezeDry(doc = window.document, {
@@ -26,6 +27,7 @@ export default async function freezeDry(doc = window.document, {
     addMetadata = true,
     keepOriginalAttributes = true,
     blobToURL,
+    fetchResource = self.fetch,
     now = new Date(),
 } = {}) {
     // Step 1: Capture the DOM (as well as DOMs inside frames).
@@ -34,7 +36,7 @@ export default async function freezeDry(doc = window.document, {
     // TODO Allow continuing processing elsewhere (background script, worker, nodejs, ...)
 
     // Step 2: Fetch subresources, recursively.
-    await maxWait(timeout)(crawlSubresourcesOfDom(resource))
+    await maxWait(timeout)(crawlSubresourcesOfDom(resource, {fetchResource}))
     // TODO Upon timeout, abort the pending fetches on platforms that support this.
 
     // Step 3: "Dry" the resources to make them static and context-free.
